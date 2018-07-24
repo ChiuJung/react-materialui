061f8a336fe61eb68522a64d88a1b567b4e32a3a
Joel <gchiujung@gmail.com>
2018-07-24 21:49:12 +0800

    Initial commit

---
+++ src/Components/Layouts/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {AppBar, Toolbar, Typography }from 'material-ui';
+
+export default props =>
+  
+  <AppBar position="static">
+    <Toolbar>
+        <Typography variant="title" color="inherit">sssd</Typography>
+    </Toolbar>
+  </AppBar>
+  
